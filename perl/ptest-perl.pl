93285ae14e827257a42544a8556a882446775317
Alex Semin <alllex.semin@gmail.com>
2014-09-11 20:30:09 +0400

    Perl addition tests (without benchmark)

--- perl/ptest-perl.pl
+++ perl/ptest-perl.pl
@@ -1,6 +1,33 @@
 #!/bin/perl
 
+# Project: ptest
+# Module:  perl
+# Author:  alllex
+# Date  :  2014-09-11
+
 use strict;
 use perl::tests::addition;
 
-params();
+# -------------------------------------------------------
+#                     Benchmark utils
+# -------------------------------------------------------
+
+
+sub benchmark {
+    my ($test, $params) = @_;
+    my @data = $params->();
+    for my $param (@data) {
+        print $test->(@$param), "\n";
+    }
+}
+
+sub mk_bunch ($) {
+    no strict 'refs';
+    sub mk_sub { \&{"perl\:\:tests\:\:$_[0]\:\:$_[1]"} }
+    (
+        mk_sub($_[0], 'test'), 
+        mk_sub($_[0], 'params')
+    );
+}
+
+benchmark(mk_bunch("addition"));
--- perl/tests/test.pm
+++ perl/tests/test.pm
@@ -3,28 +3,28 @@
 # Project: ptest
 # Module:  perl
 # Author:  alllex
-# Date  :  2014-09-07
+# Date  :  2014-09-11
 
 # -------------------------------------------------------
 #                     Addition Tests
 # -------------------------------------------------------
 
-package perl::tests::addition;
+package perl::tests::test;
 
 use strict;
 use Exporter;
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 
-use perl::tests::test;
-
 $VERSION     = 1.00;
 @ISA         = qw(Exporter);
-@EXPORT      = qw(&test &params);
-@EXPORT_OK   = qw(test params);
+@EXPORT      = qw(&get_data);
+@EXPORT_OK   = qw(get_data);
 
-sub test { $_[0] + $_[1] }
-sub params {
-    get_data('AdditionOfInt');
+sub get_data ($) {
+    my $test_name = $_[0];
+    my $file_name = "data/gen/$test_name.data";
+    open (my $file, "<", $file_name) || die "Can't read file: $!";
+    map[ split ], <$file>;
 }
 
 1;
