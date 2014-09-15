81a6b3523fdae5e64e53cc2ec1b35ae9a5d9ef4f
Alex Semin <alllex.semin@gmail.com>
2014-09-15 19:12:27 +0400

    Upgrade python printing
    Now test results from log files can be parsed and then combined to an
    report.

--- python/ptest-python.py
+++ python/ptest-python.py
@@ -4,6 +4,7 @@
 # Author:  alllex
 # Date  :  2014-09-04
 
+from tests.test import *
 from tests.addition import *
 from tests.multiplication import *
 from tests.loop import *
@@ -20,28 +21,24 @@ def benchmark(test_name, module_name, repeat=1, number=1):
     setup = 'from tests.%s import %s' % (module_name, test_name)
     test_func = test_name + '.test'
     params = eval(test_name + '.params()')
+    timed = []
     for param in params:
         sample = '%s(*%s)' % (test_func, param)
         best_time = min(benchmarker(sample, setup=setup, repeat=repeat, number=number))
-        yield (test_name, best_time, number, param)
-
-def log_benchmark(logname, bench_args):
-    with open(logname, 'a') as log:
-        timed = benchmark(*bench_args)
-        for t, bt, n, p in timed:
-            test_name = '{:^20}'.format(t)
-            best_time = '  {:0.5f}s  '.format(bt)
-            runs_count = '{:^15}'.format(n)
-            parameter = '{:^15}'.format(p)
-            to_log = '[%s] runs of [%s] took at best [%s] with parameter = [%s]\n' % (runs_count, test_name, best_time, parameter)
-            log.write(to_log)
+        timed.append((best_time, param))
+    return (
+        test_name,
+        repeat,
+        number,
+        timed
+    )
 
 def now():
     from time import strftime
     from datetime import datetime
     return datetime.now()
 
-def logname(test_name): 
+def mk_logname(test_name): 
     folder = 'results'
     lang = PythonTest.subject()
     ext  = 'log'
@@ -51,8 +48,32 @@ def tool_name():
     import sys
     return 'Python: %s' % sys.version
 
+def log_benchmark(bench_args):
+    from datetime import datetime
+    start_time = datetime.now()
+    test_name, rep, num, timed = benchmark(*bench_args)
+    finish_time = datetime.now()
+    diff = str(finish_time - start_time)
+    logname = mk_logname(test_name)
+
+    with open(logname, 'a') as log:
+        repeat = '{:^3}'.format(rep)
+        number = '{:^9}'.format(num)
+        test_name = '{:^20}'.format(test_name)
+
+        log.write('{0}\n'.format(tool_name()))
+        to_log = 'Test case({t}): repeat({r}) of number({n})\nAll {count} tests took about {total} to process'.format(
+            t=test_name, r=repeat, n=number, total=diff, count='{:^3}'.format(len(timed))
+        )
+        log.write(to_log + "\n")
+        print(to_log)
+
+        for bt, p in timed:
+            to_log = '{bt} // {p}'.format(bt='{:0.5f}'.format(bt), p=p)
+            log.write(to_log + "\n")
+
 # -------------------------------------------------------
-#                     Test runner
+#                   Test compilation
 # -------------------------------------------------------
 
 def tests():
@@ -107,37 +128,29 @@ def tests():
     slow = [
     ]
 
-    def create_bunch(repeat, number, (module_name, test_classes)):
+    def mk_bunch(repeat, number, (module_name, test_classes)):
         return [(t, module_name, repeat, number) for t in test_classes]
 
-    with open('data/gen/prefs.data', 'r') as f:
-        repeat_fast = int(f.readline())
-        repeat_mid = int(f.readline())
-        repeat_slow = int(f.readline())
-        number_fast = int(f.readline())
-        number_mid = int(f.readline())
-        number_slow = int(f.readline())
+    prefs = get_prefs()
+    repeat_fast = prefs[0]
+    repeat_mid  = prefs[1]
+    repeat_slow = prefs[2]
+    number_fast = prefs[3]
+    number_mid  = prefs[4]
+    number_slow = prefs[5]
 
     tests = []
-    for t in fast: tests += create_bunch(repeat_fast, number_fast, t)
-    for t in mid: tests += create_bunch(repeat_mid, number_mid, t)
-    for t in slow: tests += create_bunch(repeat_slow, number_slow, t)
-    return tests # tuples - (test class, module name, repeat times, runs per repeat)
+    for t in fast: tests += mk_bunch(repeat_fast, number_fast, t)
+    for t in mid: tests += mk_bunch(repeat_mid, number_mid, t)
+    for t in slow: tests += mk_bunch(repeat_slow, number_slow, t)
+    return tests # tuples - (test class, module name, repeat times, runs per test)
+
+# -------------------------------------------------------
+#                     Test runner
+# -------------------------------------------------------
 
 def main():
-    tool = tool_name()
-    for b in tests():
-        log = logname(b[0])
-        module_name = b[1]
-        test_name = b[0]
-        print('Test  : [%s] %s' % (module_name, eval(test_name + '.name()')))
-        print('Start : %s' % now())
-        with open(log, 'w+') as f: 
-            f.write('%s\n' % tool)
-            f.write('Start tests: %s\n' % now())
-        log_benchmark(log, b)
-        with open(log, 'a') as f: f.write('End tests:   %s\n' % now())
-        print('End   : %s' % now())
+    for t in tests(): log_benchmark(t)
 
 if __name__ == '__main__':
     main()
